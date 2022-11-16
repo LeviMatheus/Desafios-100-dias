e5bea60ef14bfb35c13f606e12633a3ff4ebb837
Levi Matheus <levi.matheus@outlook.com>
2022-11-15 21:37:29 -0300

    Atualização do dia 05

--- Dia 05/Dia05.py
+++ Dia 05/Dia05.py
@@ -1,38 +1,30 @@
-#Projeto gerador de senhas
+#Projeto gerador de list_senhas
 import random
 letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 simbolos = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Bem vindo ao gerador de senhas!")
-qtd_letras = int(input(f"Quantas letras você quer incluir na sua senha?\n")) 
+print("Bem vindo ao gerador de list_senhas!")
+qtd_letras = int(input(f"Quantas letras você quer incluir na sua list_senha?\n")) 
 qtd_simbolos = int(input(f"Quantos símbolos especiais?\n"))
 qtd_numeros = int(input(f"E quantos números?\n"))
 
-#zerada inicialmente
-senha = ''
+list_senha = []
+while(qtd_letras > 0):
+    list_senha.append(random.choice(letras))
+    qtd_letras-=1
+while(qtd_simbolos > 0):
+    list_senha.append(random.choice(simbolos))
+    qtd_simbolos-=1
+while(qtd_numeros > 0):
+    list_senha.append(random.choice(numeros))
+    qtd_numeros-=1
+random.shuffle(list_senha)
 
-while(qtd_letras > 0 or qtd_simbolos > 0 or qtd_numeros > 0):
-    categoria = random.randrange(0,3)
-    if(categoria==0):
-        if(qtd_letras == 0):
-            continue
-        else:
-            senha += random.choice(letras)
-            qtd_letras-=1
-    elif(categoria==1):
-        if(qtd_simbolos == 0):
-            continue
-        else:
-            senha += random.choice(simbolos)
-            qtd_simbolos-=1
-    elif(categoria==2):
-        if(qtd_numeros == 0):
-            continue
-        else:
-            senha += random.choice(numeros)
-            qtd_numeros-=1
+senha = ""
+for caractere in list_senha:
+    senha+= caractere
 
-print(f"Sua senha gerada é: {senha}")
+print(f"Sua list_senha gerada é: {senha}")
 
 
